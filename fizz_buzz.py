8a9e4f0b60e48e3190817221464a869bfc93a53e
AlexHerbertGit <alex.herbert.tech@gmail.com>
2024-07-31 11:03:24 +1200

    Added while loop to FizzBuzz function.

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,7 +1,8 @@
 
 def fizz_buzz(up_to_number):
     answer = ""
-    for i in range(up_to_number):
+    i = 0
+    while i < up_to_number:
         if i % 3 == 0 and i % 5 == 0 :
             answer = "FizzBuzz"
         elif i % 3 == 0:
@@ -13,6 +14,7 @@ def fizz_buzz(up_to_number):
         
         print(i, answer)
         
+        i += 1
 if __name__ == "__main__":
     fizz_buzz(25)
             
